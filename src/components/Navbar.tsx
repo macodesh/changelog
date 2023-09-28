6073fd9512ead474917b3fde9465d4554ad288c6
Marlon Couto <devpatola@proton.me>
2023-09-28 17:28:22 -0300

    Atualiza classes da Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,14 @@ export default function Navbar() {
   return (
     <header>
       <nav className="navbar">
-        <div className="navbar__logo">
+        <div className="navbar-logo">
           <img
             src="https://picsum.photos/300/300"
             alt="logo"
           />
         </div>
 
-        <ul className="navbar__links">
+        <ul className="navbar-links">
           <li>
             <a href="#">GitHub</a>
           </li>
