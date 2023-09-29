3cd9e01915afc50785292c9a13403cfbfc179ed4
Marlon Couto <devpatola@proton.me>
2023-09-29 18:00:04 -0300

    Cria arquivo data.ts

---
+++ src/data.ts
@@ -0,0 +1,92 @@
+const data = [
+  {
+    id: '1',
+    status: 'Em progresso',
+    tag: 'v1.0',
+    title: 'Múltiplos casos de uso',
+    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia itaque eos blanditiis unde animi? Perferendis eos quibusdam sint. Ipsum sequi fuga dicta est aliquid vero eius ex! Consequatur, non. Debitis placeat id laboriosam quo iste vitae voluptas eveniet minus soluta?',
+    preview: 'https://picsum.photos/300/200',
+    updates: [
+      {
+        id: '1',
+        label: 'feature',
+        title: 'Novas features',
+        topics: [
+          {
+            id: '1',
+            title: 'Feature 1',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, quidem!'
+          },
+          {
+            id: '2',
+            title: 'Feature 2',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, quidem!'
+          }
+        ]
+      },
+      {
+        id: '2',
+        label: 'bug',
+        title: 'Hot fixes de Setembro',
+        topics: [
+          {
+            id: '1',
+            title: 'Correções de bugs',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, quidem!'
+          },
+          {
+            id: '2',
+            title: 'Melhorias de performance',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, quidem!'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: '2',
+    status: 'Em progresso',
+    tag: 'v1.0',
+    title: 'Múltiplos casos de uso',
+    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia itaque eos blanditiis unde animi? Perferendis eos quibusdam sint. Ipsum sequi fuga dicta est aliquid vero eius ex! Consequatur, non. Debitis placeat id laboriosam quo iste vitae voluptas eveniet minus soluta?',
+    preview: 'https://picsum.photos/300/200',
+    updates: [
+      {
+        id: '1',
+        label: 'feature',
+        title: 'Novas features',
+        topics: [
+          {
+            id: '1',
+            title: 'Feature 1',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, quidem!'
+          },
+          {
+            id: '2',
+            title: 'Feature 2',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, quidem!'
+          }
+        ]
+      },
+      {
+        id: '2',
+        label: 'bug',
+        title: 'Hot fixes de Setembro',
+        topics: [
+          {
+            id: '1',
+            title: 'Correções de bugs',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, quidem!'
+          },
+          {
+            id: '2',
+            title: 'Melhorias de performance',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, quidem!'
+          }
+        ]
+      }
+    ]
+  }
+];
+
+export default data;
